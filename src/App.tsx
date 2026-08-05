7147dd2d05ad7f833b290faa4aac956e2147c2cc
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-08-05 20:35:34 -0300

    feat: create mainTemplate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,8 @@
-import { Container } from './components/Container'
-import { CountDown } from './components/CountDown'
-import { DefaultInput } from './components/DefaultInput'
-import { Logo } from './components/Logo'
-import { Menu } from './components/Menu'
+import { Home } from './pages/Home'
 import './styles/global.css'
 import './styles/theme.css'
 
 export default function App() {
 
-    return (
-        <>
-            <Container>
-                <Logo />
-            </Container>
-
-            <Container>
-                <Menu />
-            </Container>
-
-            <Container>
-                <CountDown />
-            </Container>
-
-
-            <Container>
-                <form className='form'>
-                    <div className="formRow">
-                        <DefaultInput id='input' type='text' labelText='Task' placeholder='Digite algo' defaultValue={'Valor preenchido'} />
-                    </div>
-                    <div className="formRow">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0000000</p>
-                    </div>
-                    <div className="formRow">
-                        <button>
-                            Enviar
-                        </button>
-                    </div>
-                </form>
-            </Container>
-        </>
-    )
+    return <Home />
 }
